51f02868f758f09658e306da1427837ed4525525
Pragyesh Singh Chauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2025-04-13 01:43:22 +0000

    build fix

--- components/background.tsx
+++ components/background.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useCallback } from "react";
-import Particles from "react-particles";
+import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim";
 import type { Engine } from "tsparticles-engine";
 import { useTheme } from "next-themes";
